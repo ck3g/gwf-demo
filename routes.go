57b94a239c7867c674d7bb58ce4cf25e4c3b13b0
Vitali Tatarintev <Kalastiuz@gmail.com>
2022-01-04 10:04:52 +0100

    Add form validation example

--- routes.go
+++ routes.go
@@ -22,6 +22,9 @@ func (a *application) routes() *chi.Mux {
 	a.App.Routes.Post("/users/login", a.Handlers.PostUserLogin)
 	a.App.Routes.Get("/users/logout", a.Handlers.UserLogout)
 
+	a.App.Routes.Get("/form", a.Handlers.Form)
+	a.App.Routes.Post("/form", a.Handlers.PostForm)
+
 	a.App.Routes.Get("/create-user", func(w http.ResponseWriter, r *http.Request) {
 		u := data.User{
 			FirstName: "John",
@@ -65,6 +68,16 @@ func (a *application) routes() *chi.Mux {
 
 		u, _ := a.Models.Users.Get(id)
 		u.LastName = a.App.RandomString(10)
+
+		validator := a.App.Validator(nil)
+
+		u.Validate(validator)
+
+		if !validator.Valid() {
+			fmt.Fprintf(w, "failed validation")
+			return
+		}
+
 		err := u.Update(*u)
 		if err != nil {
 			a.App.ErrorLog.Println(err)
