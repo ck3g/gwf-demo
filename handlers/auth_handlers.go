0f77765fbbb3e312c1a3e35442a87dd4789c08b6
Vitali Tatarintev <Kalastiuz@gmail.com>
2022-01-17 11:23:32 +0100

    Display reset password form

--- handlers/auth_handlers.go
+++ handlers/auth_handlers.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/CloudyKit/jet/v6"
 	"github.com/ck3g/gwf/mailer"
 	"github.com/ck3g/gwf/urlsigner"
 )
@@ -176,4 +177,42 @@ func (h *Handlers) PostForgot(w http.ResponseWriter, r *http.Request) {
 
 	// redirect the user
 	http.Redirect(w, r, "/users/login", http.StatusSeeOther)
+}
+
+func (h *Handlers) ResetPasswordForm(w http.ResponseWriter, r *http.Request) {
+	// get form values
+	email := r.URL.Query().Get("email")
+	theURL := r.RequestURI
+	testURL := fmt.Sprintf("%s%s", h.App.Server.URL, theURL)
+
+	// validate the url
+	signer := urlsigner.Signer{
+		Secret: []byte(h.App.EncryptionKey),
+	}
+
+	valid := signer.VerifyToken(testURL)
+	if !valid {
+		h.App.ErrorLog.Println("Invalid url")
+		h.App.ErrorUnauthorized(w)
+		return
+	}
+
+	// make sure it's not expired
+	expired := signer.Expired(testURL, 60)
+	if expired {
+		h.App.ErrorLog.Println("Link expired")
+		h.App.ErrorUnauthorized(w)
+		return
+	}
+
+	// display the form
+	encryptedEmail, _ := h.encrypt(email)
+
+	vars := make(jet.VarMap)
+	vars.Set("email", encryptedEmail)
+
+	err := h.render(w, r, "reset-password", vars, nil)
+	if err != nil {
+		return
+	}
 }
